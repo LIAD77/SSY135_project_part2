2538f3905e4d9b2733eb4d37382ebc91ec43c558
Albin Severinson <albin@severinson.org>
2017-02-13 14:20:43 +0100

    Added functions to parallelize and serialize data

---
+++ parallelize.m
@@ -0,0 +1,20 @@
+function parallel = parallelize(serial, channels)
+%PARALLELIZE Parallelize a vector by reshaping it into a vector
+%with channels rows. The matrix is filled column-wise.
+% serial - Input vector.
+% channels - The number of channels (rows of the matrix).
+
+if channels > numel(serial)
+    error('There must be fewer channels than the length of the input.')
+end
+
+if numel(serial) ~= size(serial, 1)
+    error('Input must be a column vector.')
+end
+
+% Append zeroes if the numel of the input is not a multiple of the
+% number of channels.
+serial = [serial ; zeros(channels - mod(numel(serial), channels), 1)];
+parallel = reshape(serial, channels, numel(serial) / channels);
+
+end
---
+++ serialize.m
@@ -0,0 +1,8 @@
+function serial = serialize(parallel)
+%SERIALIZE Serialize a vector by reshaping it into a column vector
+%by taking elements column-wise from the matrix.
+% parallel - Input matrix.
+
+serial = reshape(parallel, numel(parallel), 1);
+
+end
