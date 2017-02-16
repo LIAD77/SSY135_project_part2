f9a33569f578d63c3cf58c8b533e0ce0e77bcf3f
Martin Yang <daoyuan@student.chalmers.se>
2017-02-16 20:59:57 +0100

    remove useless parameters

--- ofdm_sym_gen.m
+++ ofdm_sym_gen.m
@@ -1,4 +1,4 @@
-function [ z ] = ofdm_sym_gen( b,N,Ncp,mod_type,E,Ts )
+function [ z ] = ofdm_sym_gen( b,N,Ncp,mod_type )
 %OFDM_SYM Summary of this function goes here
 %   Detailed explanation goes here
 %   generate one ofdm symbol
@@ -14,7 +14,7 @@ bs = reshape(b(:),N,log2(mod_type));
 sb = bi2de(bs);
 %map decimal to MQAM symbol
 %don't need to calculate k_norm here
-sp = sqrt(E) * qammod(sb,mod_type,'UnitAveragePower',true);
+sp = qammod(sb,mod_type,'UnitAveragePower',true);
 %ifft
 z = sqrt(N) * ifft(sp);
 %add cp
