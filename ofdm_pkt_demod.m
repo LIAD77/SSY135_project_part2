7167d1f99d5a77cf498cf1f52cfb785df87969df
Martin Yang <daoyuan@student.chalmers.se>
2017-02-16 20:59:37 +0100

    ofdm packet demod

---
+++ ofdm_pkt_demod.m
@@ -0,0 +1,8 @@
+function [ b_hat ] = ofdm_pkt_demod( rx_signal )
+%OFDM_PKT_DEMOD Summary of this function goes here
+%   Detailed explanation goes here
+%   demodulate ofdm packet, dose all the reshaping, 
+
+
+end
+
